ef29a77d04c6fb717ab05b32779ad07cf2f4fa0c
Akash Kothari <akashk4@illinois.edu>
2022-03-16 15:21:11 -0500

    Update and rename leetcode.txt to leetcode.py

--- leetcode.py
+++ leetcode.py
@@ -1,11 +1,8 @@
 
-Question: Given a linked list, swap every two adjacent nodes and return its head. 
-You must solve the problem without modifying the values in the list's nodes
-(i.e., only nodes themselves may be changed.)
-
-
-My solution:
-
+# Question: Given a linked list, swap every two adjacent nodes and return its head. 
+# You must solve the problem without modifying the values in the list's nodes
+# (i.e., only nodes themselves may be changed.)
+# My solution:
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
