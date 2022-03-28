37701bb66acf4fb51d03f6737f97ec808f9f6750
Akash Kothari <akashk4@illinois.edu>
2022-03-28 16:50:55 -0500

    Update amazon.py

--- amazon.py
+++ amazon.py
@@ -59,7 +59,64 @@ class Solution:
             return ListNode(sum)
         return start
             
+ 
+# You are given an array of k linked-lists lists, each linked-list is sorted in ascending order.
+# Merge all the linked-lists into one sorted linked-list and return it. 
+# Link: https://leetcode.com/problems/merge-k-sorted-lists/
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        def printList(list1):
+            print("PRINTING LIST:")
+            node = list1
+            while node != None:
+                print(node.val)
+                node = node.next
             
-    
+        def insertlistinto(list1, list2):
+            head1 = list1
+            head2 = list2
+            cur_node2 = head2
+            while cur_node2 != None:
+                cur_node1 = head1
+                prev_node1 = None
+                while cur_node1 != None and cur_node1.val < cur_node2.val:
+                    prev_node1 = cur_node1
+                    cur_node1 = cur_node1.next
+                head2 = cur_node2.next
+                cur_node2.next = cur_node1
+                if head1 == cur_node1:
+                    head1 = cur_node2
+                else:
+                    prev_node1.next = cur_node2
+                cur_node2 = head2
+            return head1
+        
+        def merge2Lists(list1 : Optional[ListNode], list2 : Optional[ListNode]) -> Optional[ListNode]:
+            if list1.val > list2.val:
+                return insertlistinto(list2, list1)
+            return insertlistinto(list1, list2)
+        
+        if len(lists) == 0:
+            return None
+        index = 0
+        result = None
+        for array in lists:
+            if array == None:
+                index += 1
+                continue
+            result = lists[index]
+            break
+        if index != len(lists) - 1:
+            for array in lists[index + 1:]:
+                if array == None:
+                    continue
+                result = merge2Lists(result, array)
+        return result
+                    
       
       
