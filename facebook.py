cc2988603ed573d158c3538e9bd0e02046bf8694
Akash Kothari <akashk4@illinois.edu>
2022-03-29 14:16:49 -0500

    Create facebook.py

---
+++ facebook.py
@@ -0,0 +1,22 @@
+# First one was to write an algorithm that traverses a tree of digits starting 
+# at the root and calculates the sum of the numbers that are formed by each path. For example if the tree was:
+#        5
+#       / \
+#      4   2
+#     / \
+#   1   3
+#the algorithm should calculate 541 + 543 + 52.
+
+
+
+
+
+
+
+# Write an algorithm that would take an array representing a number, 
+# and find the largest number that can be created by swapping only two 
+# digits. For example if the input was [1,2,3,4,5]  the algorithm should 
+# swap the 1 and the 5 and output [5,2,3,4,1].
+
+
+
