92cddca1adfebf12f81c5f4f2a8aaca46ad05d5a
Akash Kothari <akashk4@illinois.edu>
2022-03-29 01:38:14 -0500

    Update amazon.py

--- amazon.py
+++ amazon.py
@@ -162,4 +162,39 @@ class MinStack:
 # obj.pop()
 # param_3 = obj.top()
 # param_4 = obj.getMin()
-      
+
+
+
+
+# Design a data structure that follows the constraints of a Least Recently Used (LRU) cache.
+# Implement the LRUCache class:
+# LRUCache(int capacity) Initialize the LRU cache with positive size capacity.
+# int get(int key) Return the value of the key if the key exists, otherwise return -1.
+# void put(int key, int value) Update the value of the key if the key exists. 
+# Otherwise, add the key-value pair to the cache. If the number of keys exceeds the 
+# capacity from this operation, evict the least recently used key.
+# The functions get and put must each run in O(1) average time complexity.
+# Link: https://leetcode.com/problems/lru-cache/
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.cache = collections.OrderedDict()
+        self.capacity = capacity
+
+    def get(self, key: int) -> int:
+        if key not in self.cache:
+            return -1
+        self.cache.move_to_end(key)
+        return self.cache[key]
+
+    def put(self, key: int, value: int) -> None:
+        if key not in self.cache:
+            if len(self.cache) == self.capacity:
+                self.cache.popitem(last=False)
+        self.cache[key] = value
+        self.cache.move_to_end(key)
+        
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
